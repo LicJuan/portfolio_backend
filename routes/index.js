902aa4f89fa9b6cc91b28d42f06c3ef44cc4a7c4
LicJuan <jerg.esteban@gmail.com>
2021-08-25 10:19:12 -0400

    Route modified

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import UserRoutes from './UserRoutes'
+import CategoryRoutes from './CategoryRoutes'
 import AuthRoutes from './AuthRoutes'
 const router = Router()
 
 router.use('/users', UserRoutes)
+router.use('/categories', CategoryRoutes)
 router.use('/auth', AuthRoutes)
 
 export default router
